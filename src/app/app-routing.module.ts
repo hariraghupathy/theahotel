b02099abbf868f76a4445c4e9d1e5d709648e04e
harikrishnan <hariraghupathy@gmail.com>
2018-10-23 04:30:32 +0530

    Initial Commit, created routing module, added hotel pages, added paths

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { AccomodationComponent } from './accomodation/accomodation.component';
+import { CelebrationsComponent } from 'src/app/celebrations/celebrations.component';
+import { FoodBeverageComponent } from 'src/app/food-beverage/food-beverage.component';
+import { LifestyleComponent } from 'src/app/lifestyle/lifestyle.component';
+import { OffersComponent } from 'src/app/offers/offers.component';
+import { WeddingsComponent } from 'src/app/weddings/weddings.component';
+import { SigninComponent } from 'src/app/auth/signin/signin.component';
+import { SignupComponent } from 'src/app/auth/signup/signup.component';
+const routes: Routes = [
+{path: 'accomodation', component : AccomodationComponent },
+{path: 'celebrations', component : CelebrationsComponent},
+{path: 'food-and-beverage', component : FoodBeverageComponent},
+{path: 'lifestyle', component: LifestyleComponent},
+{path: 'weddings', component : WeddingsComponent },
+{path: 'offers' , component : OffersComponent } ,
+{path: 'signin' , component : SigninComponent },
+{path: 'signup' , component : SignupComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
